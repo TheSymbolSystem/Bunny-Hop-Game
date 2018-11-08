3b448b9b66af33997e2a60633b913eb793791fa8
TheSymbolSystem <44449146+TheSymbolSystem@users.noreply.github.com>
2018-11-08 19:46:18 -0400

    Add files via upload

---
+++ GrassPlatform.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+const SPRING_CHANCE = 5
+onready var sprite = $Sprite
+var sprite_half_width
+var spring_path = "res://Spring.tscn"
+#If in folder, do '*FolderName*/' to link scene
+
+func _ready():
+	randomize()
+	connect("body_entered", self, "_on_body_entered")
+	sprite_half_width = sprite.texture.get_size().x / 2 * scale.x
+	if rand_range(0, 100) > 100 - SPRING_CHANCE:
+		var new_spring = load(spring_path).instance()
+		add_child(new_spring)
+		new_spring.position = Vector2(0, -new_spring.height)
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		if body.position.y < position.y:
+			body.jump()
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
---
+++ Player.gd
@@ -0,0 +1,90 @@
+extends KinematicBody2D
+
+onready var animated_sprite = $AnimatedSprite
+onready var audio_jump = $AudioJump
+
+const GRAVITY = 1500
+const GRAVITY_INCREMENT = 2500
+const JUMP_DECREMENT = 100
+const JUMP_FORCE = 40
+
+export var speed = 500
+
+var screen_width
+var half_sprite_width
+
+var jumping = false
+var current_jump_force = 0
+var current_gravity = 0
+var highest_reached_position = 300
+var death_position_offset = 1200
+var score = 0
+
+signal just_jumped
+
+func _ready():
+	score = abs(highest_reached_position) - 300
+	screen_width = get_viewport_rect().size.x
+	half_sprite_width = animated_sprite.frames.get_frame("idle", 0).get_width() / 2
+	current_gravity = GRAVITY
+
+func _process(delta):
+	if !jumping:
+		_increment_gravity(delta)
+		position.y += current_gravity * delta
+	else:
+		position.y -= current_jump_force
+		_decrement_jump(delta)
+	highest_reached_position = position.y if position.y < highest_reached_position else highest_reached_position
+	score = int(abs(highest_reached_position) - 300)
+	score = score if score > 0 else 0
+	if position.y >= highest_reached_position + death_position_offset:
+		die()
+	
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed * delta
+	elif Input.is_action_pressed("ui_right"):
+		position.x += speed * delta
+	elif Input.is_action_pressed("ui_accept"):
+		jump()
+
+	_check_boundaries()
+
+func jump():
+	if jumping:
+		return
+	current_gravity = 0
+	jumping = true
+	current_jump_force = JUMP_FORCE
+	animated_sprite.play("jump")
+	emit_signal("just_jumped")
+	audio_jump.play()
+
+func add_impulse(impulse):
+	emit_signal("just_jumped")
+	current_gravity = 0
+	jumping = true
+	current_jump_force = impulse
+	animated_sprite.play("jump")
+
+func die():
+	$"/root/PlayerData".save_highscore(score)
+	$"/root/LevelManager".change_scene("Menu")
+
+func _increment_gravity(delta):
+	current_gravity += GRAVITY_INCREMENT * delta
+	if current_gravity >= GRAVITY:
+		current_gravity = GRAVITY
+
+func _decrement_jump(delta):
+	current_jump_force -= JUMP_DECREMENT * delta
+	if current_jump_force <= 0:
+		current_jump_force = 0
+		jumping = false
+		animated_sprite.play("idle")
+
+func _check_boundaries():
+	if position.x > screen_width:
+		position.x = 0
+	elif position.x < 0:
+		position.x = screen_width
